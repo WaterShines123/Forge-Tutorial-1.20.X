26fedb5c4e33387a35a2b5ac12b0e50093d7fa7d
WaterShines123 <129415710+WaterShines123@users.noreply.github.com>
2024-09-02 17:50:45 +0100

    Adding custom tooltips

--- src/main/java/net/watershines/tutorialmod/block/custom/SoundBlock.java
+++ src/main/java/net/watershines/tutorialmod/block/custom/SoundBlock.java
@@ -1,6 +1,7 @@
 package net.watershines.tutorialmod.block.custom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.TickTask;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
@@ -8,6 +9,9 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -15,7 +19,9 @@ import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.Random;
 
 public class SoundBlock extends Block {
@@ -86,5 +92,9 @@ public class SoundBlock extends Block {
 
     }
 
-
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable BlockGetter pLevel, List<Component> pTooltip, TooltipFlag pFlag) {
+        pTooltip.add(Component.literal("Plays sounds when right clicked or powered"));
+        super.appendHoverText(pStack, pLevel, pTooltip, pFlag);
+    }
 }
